18a69e7438c389cf3942e2525fa0b97664c87a69
rxjang <rxforp@naver.com>
2020-11-24 23:10:39 +0900

    update & delete

--- src/components/Read.vue
+++ src/components/Read.vue
@@ -6,7 +6,7 @@
                 <td>제목</td>
                 <td>내용</td>
             </tr>
-            <tr :key="index" v-for="(value,index) in data">
+            <tr :key="index" v-for="(value,index) in data" @click="detail(index)">
                 <td>{{value.writer}}</td>
                 <td>{{value.title}}</td>
                 <td>{{value.content}}</td>
@@ -28,6 +28,14 @@ export default {
         write(){
             this.$router.push({
                 path:'create'
+            })
+        },
+        detail(index){
+            this.$router.push({
+                name:'Detail',
+                params:{
+                    contentId:index
+                }
             })
         }
     }

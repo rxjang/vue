a7909c8bde60a3cd6b7b9a8a2b2f30fa8cd42a58
rxjang <rxforp@naver.com>
2020-11-25 16:59:53 +0900

    add wirter

--- bootstrap-bbs/src/components/Board.vue
+++ bootstrap-bbs/src/components/Board.vue
@@ -7,6 +7,8 @@
 <script>
 import data from '@/data'
 
+let items = data.Content.sort((a,b) => {return b.content_id-a.content_id})
+items =items.map(contentItem=>{return {...contentItem,user_name:data.User.filter(userItem=>userItem.user_id===contentItem.user_id)[0].name}})
   export default {
     data() {
       return {
@@ -20,11 +22,15 @@ import data from '@/data'
                 label:'제목'
             },
             {
+                key:'user_name',
+                label:'글쓴이'
+            },
+            {
                 key:'created_at',
                 label:'작성일'
             },
         ],
-        items: data.Content
+        items: items
       }
     }
   }

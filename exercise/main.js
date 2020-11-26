1d0028e59bb3add797fa1f2048b74e76cf45652d
rxjang <rxforp@naver.com>
2020-11-26 21:45:55 +0900

    Attrivute Binding

--- exercise/main.js
+++ exercise/main.js
@@ -2,7 +2,10 @@ var app = new Vue({
     el: '#app',
     data:{
         product:"Socks",
-        description:"A pair of warm, fuzzy socks"
+        image:"./assests/vmSocks-green-onWhite.jpg",
+        description:"A pair of warm, fuzzy socks",
+        altText:"A pair of socks",
+        link:"https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg"
     }
 
 })

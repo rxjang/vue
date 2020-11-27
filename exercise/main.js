d4f735251ed370c129d6804e34b90096047ba0a2
rxjang <rxforp@naver.com>
2020-11-27 12:02:46 +0900

    Conditional Rendering

--- exercise/main.js
+++ exercise/main.js
@@ -5,7 +5,9 @@ var app = new Vue({
         image:"./assests/vmSocks-green-onWhite.jpg",
         description:"A pair of warm, fuzzy socks",
         altText:"A pair of socks",
-        link:"https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg"
+        link:"https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
+        inventory:0,
+        onSale:true
     }
 
-})
+})  

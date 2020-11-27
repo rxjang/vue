6f86ec68078bb16385a7e395b710bed6bd1ded6f
rxjang <rxforp@naver.com>
2020-11-27 12:11:18 +0900

    List Rendering

--- exercise/main.js
+++ exercise/main.js
@@ -7,7 +7,19 @@ var app = new Vue({
         altText:"A pair of socks",
         link:"https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
         inventory:0,
-        onSale:true
+        onSale:true,
+        details:["80% cotton","20% polyester","Gender-netural"],
+        variants:[
+            {
+                variantId:2234,
+                variantColor:"green"
+            },
+            {
+                variantId:2235,
+                variantColor:"blue"
+            }
+        ],
+        sizes:["XS","S","M","L"]
     }
 
 })  
